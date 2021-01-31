7b120e97ecd0bbb9cad09102dc6653999a4304a3
HenzelMoras <henzelmoras@gmail.com>
2021-01-31 09:42:09 -0500

    registered Post model to admin site

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from . models import Post  # added Post model to regiter in Django admin site
+from . models import Post  # added Post model to regiter in Django admin site 
 
 # Register your models here.
 
-admin.site.register(Post)
+# admin.site.register(Post)
+
+@admin.register(Post)  # Decorator to register
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
