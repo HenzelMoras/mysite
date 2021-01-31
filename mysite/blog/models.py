101493b1c61fa6f3a2239d73f9b32b14b323d323
HenzelMoras <henzelmoras@gmail.com>
2021-01-31 13:19:33 -0500

    imported reverse module for get_absoulte_url

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone  # for time related objects like publish, created, updated
 from django.contrib.auth.models import User  # to be used to link the users to author using foreign key 
+from django.urls import reverse
 # Create your models here.
 
 class PublishedManager(models.Manager):
@@ -35,6 +36,13 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-publish',)  # reverse ordering filtered using publish object for displaying the latest posts
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
 
     def __str__(self):
         return self.title                          
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,7 +6,8 @@ app_name = 'blog'
  
 urlpatterns = [
     # post views
-    path('', views.post_list, name='post_list'),  # name to be referred for including in templates "app_name:name(of the view like name='post_list)"
+    # path('', views.post_list, name='post_list'),  # name to be referred for including in templates "app_name:name(of the view like name='post_list)"
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail, name='post_detail'),
 ]
