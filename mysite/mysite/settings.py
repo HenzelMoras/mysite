1dc3be49f173859d4628ab008ef67603d659553b
HenzelMoras <henzelmoras@gmail.com>
2021-02-02 04:58:57 -0500

    added share post by creating urls,forms,views,templates for the same

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Email Config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'your_account@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
