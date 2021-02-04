dea17bd27a8105464622f8d7906183606b8382b9
HenzelMoras <henzelmoras@gmail.com>
2021-02-04 02:13:42 -0500

    tweaked the search enginine to search TrigramSimilarity

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,7 +6,7 @@ from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 
 # Create your views here.
 
@@ -120,8 +120,8 @@ def post_search(request):
         if form.is_valid():
             query = form.cleaned_data['query']
             results = Post.objects.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.3).order_by('-similarity')
     return render(request,
                   'blog/post/search.html',
                   {'form': form,
